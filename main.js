f908f03a67abb8d8d5100f46f527554bc7b31d90
Shruthic27 <76080986+Shruthic27@users.noreply.github.com>
2022-01-22 18:20:41 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-song=""
+song1=""
 song2=""
 lwx=0;
 lwy=0;
 rwx=0;
 rwy=0;
+slw=0;
+song1_status="";
+song2_status="";
 
 function preload(){
-song= loadSound("music.mp3")
-song2= loadSound("music2.mp3")
+song1= loadSound("music.mp3");
+song2= loadSound("music2.mp3");
 }
 
 function setup(){
-    canvas=createCanvas(600,500)
-    canvas.center()
+canvas=createCanvas(600,500)
+canvas.center()
 
-    video= createCapture(VIDEO);
-    video.hide();
-    
-    poseNet=ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
-    }
-    function modelLoaded(){
-        console.log("PoseNet Is Initialized")
-    
+video= createCapture(VIDEO);
+video.hide();
+
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
 }
 function modelLoaded(){
     console.log("PoseNet Is Initialized")
@@ -31,6 +30,9 @@ function modelLoaded(){
 function gotPoses(results){
 if(results.length>0){
 console.log(results);
+slw=results[0].pose.keypoints[9].score;
+srw=results[0].pose.keypoints[9].score;
+console.log("slw = "+slw+"srw = "+srw);
 
 lwx=results[0].pose.leftWrist.x;
 lwy=results[0].pose.leftWrist.y;
@@ -43,7 +45,24 @@ console.log("rwx="+rwx+"rwy="+rwy);
 }
 
 function draw(){
-    image(video, 0, 0, 600, 500)
+    image(video, 0, 0, 600, 500);
+
+    song1_status = song1.isplaying();
+
+    song2_status = song2.isplaying();
+
+    fill("#0000ff");
+    stroke("#0000ff");
+
+    if(slw>0.2){
+    circle(lwx,lwy,20);
+    song1.stop
+    }
+
+    if(srw>0.2){
+        circle(rwx,rwy,20);
+        song2.stop
+        }
 }
 
 function play(){
