46ef26028fa91d91230608accebfc85a8f5d361a
Shruthic27 <76080986+Shruthic27@users.noreply.github.com>
2022-01-29 19:04:01 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-song1=""
-song2=""
+song1="";
+song2="";
 lwx=0;
 lwy=0;
 rwx=0;
@@ -25,14 +25,14 @@ poseNet=ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
 }
 function modelLoaded(){
-    console.log("PoseNet Is Initialized")
+    console.log("PoseNet Is Initialized");
 }
 
 function gotPoses(results){
 if(results.length>0){
 console.log(results);
 slw=results[0].pose.keypoints[9].score;
-srw=results[0].pose.keypoints[9].score;
+srw=results[0].pose.keypoints[10].score;
 console.log("slw = "+slw+"srw = "+srw);
 
 lwx=results[0].pose.leftWrist.x;
@@ -48,34 +48,28 @@ console.log("rwx="+rwx+"rwy="+rwy);
 function draw(){
     image(video, 0, 0, 620, 520);
 
-    song1_status = song1.isplaying();
+    song1_status = song1.isPlaying();
 
-    song2_status = song2.isplaying();
+    song2_status = song2.isPlaying();
 
     fill("#0000ff");
     stroke("#0000ff");
 
     if(slw>0.2){
     circle(lwx,lwy,20);
-    song1.stop
-    document.getElementById("Song").innerHTML="Song 2 is playing"
-    song2.play
-
-    if (song1.isPlaying=false) {
-        document.getElementById("Song").innerHTML="Song 2 is playing"
-        song2.play  
+    song1.stop();
+    if (song2_status==false) {
+        song2.play();
+        document.getElementById("Song").innerHTML="Peter Pan song is playing";
     }
     }
 
     if(srw>0.2){
         circle(rwx,rwy,20);
-        song2.stop
-        document.getElementById("Song").innerHTML="Song 1 is playing"
-        song1.play
-
-        if (song2.isPlaying=false) {
-            document.getElementById("Song").innerHTML="Song 1 is playing"
-            song1.play  
+        song2.stop();
+        if (song1_status==false) {
+            song1.play();
+            document.getElementById("Song").innerHTML="Harry Potter song is playing";
         }
         }
 }
